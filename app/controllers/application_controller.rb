6191ff3b130de468f65134155057593f07f1b154
elarhadu <elarhadu@gmail.com>
2023-04-24 22:59:32 +0000

    Add authentication filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    default_parameters = %i[name role]
+    devise_parameter_sanitizer.permit(:sign_up, keys: default_parameters)
+    devise_parameter_sanitizer.permit(:sign_in, keys: default_parameters)
+    devise_parameter_sanitizer.permit(:account_update, keys: default_parameters)
+  end
 end
