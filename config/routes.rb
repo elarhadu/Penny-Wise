4794c4d896e06126ce9521b1e961e504c1cb04c3
elarhadu <elarhadu@gmail.com>
2023-04-28 19:55:26 +0000

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get "home/index"
+  resources :groups do
+    resources :expenses
+  end
   devise_for :users
-  root to: "home#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'groups#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root "home#index", as: :unauthenticated_root
+    end
+  end
 end
